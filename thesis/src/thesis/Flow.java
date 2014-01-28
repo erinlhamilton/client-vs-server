cbf738b9f43c9158e1fc7c9768013ee88fda9db0
Erin Hamilton <erin@erinhamilton.me>
2014-01-28 12:57:52 -0600

    Test commit

--- thesis/src/thesis/Flow.java
+++ thesis/src/thesis/Flow.java
@@ -23,41 +23,52 @@ public class Flow {
 	 *
 	 */
 	public String runServer(int testID){
-		int wktIDs[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int unionIDs[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	//public static void main(String[] args){
+		//int testID = 0;
+		
+		int wktIDs[] = {10, 20, 30, 40, 50, 60, 70,	80,	90,	100, 200, 300, 400, 500, 600,
+			700, 800, 900, 1000};
+		
+//		int wktIDs[] = {10, 20, 30,	40,	50,	60,	70,	80,	90,	100, 200, 300, 400, 500, 600,
+//				700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
+//				20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 150000, 
+//				200000, 250000, 300000, 350000, 400000, 450000, 500000};
+
+		//int unionIDs[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		String results = "";
-		Storage storeResults = new Storage();
+		//Storage storeResults = new Storage();
 		
 		//Buffer points
 		for (int i = 0; i < wktIDs.length; i++){
-			results = callBuff(testID, "points", wktIDs[i]);
+			results = callBuff(testID, "Points", wktIDs[i]);
 			System.out.println(results);
 			//storeResults.insertResults(results);
 		}
 		
-		//Buffer lines
-		for (int i = 0; i < wktIDs.length; i++){
-			results = callBuff(testID, "lines", wktIDs[i]);
-			storeResults.insertResults(results);
-		}
-		
-		//Buffer polygons
-		for (int i = 0; i < wktIDs.length; i++){
-			results = callBuff(testID, "polygons", wktIDs[i]);
-			storeResults.insertResults(results);
-		}
-		
+//		//Buffer lines
+//		for (int i = 0; i < wktIDs.length; i++){
+//			results = callBuff(testID, "Lines", wktIDs[i]);
+//			storeResults.insertResults(results);
+//		}
+//		
+//		//Buffer polygons
+//		for (int i = 0; i < wktIDs.length; i++){
+//			results = callBuff(testID, "Polygons", wktIDs[i]);
+//			storeResults.insertResults(results);
+//		}
+//		
 		//Triangulate points
-		for (int i = 0; i < wktIDs.length; i++){
-			results = callTriangulation(testID, wktIDs[i]);
-			storeResults.insertResults(results);
-		}
-		
-		//Union two polygons
-		for (int i = 0; i < unionIDs.length; i++){
-			results = callUnion(testID, unionIDs[i]);
-			storeResults.insertResults(results);
-		}
+//		for (int i = 0; i < wktIDs.length; i++){
+//			results = callTriangulation(testID, wktIDs[i]);
+//			System.out.println(results);
+//			//storeResults.insertResults(results);
+//		}
+//		
+//		//Union two polygons
+//		for (int i = 0; i < unionIDs.length; i++){
+//			results = callUnion(testID, unionIDs[i]);
+//			storeResults.insertResults(results);
+//		}
 		
 		return "Test Complete.";
 	}
@@ -123,7 +134,7 @@ public class Flow {
 	 */
 	public String callTriangulation(int testID, int wktID){
 		String geoprocess = "Triangulation";
-		String dataType = "points";
+		String dataType = "Points";
 
 		//Retrieve data from db
 		Storage storageData = new Storage();
@@ -176,13 +187,13 @@ public class Flow {
 	 */
 	public String callUnion(int testID, int wktID){
 		String geoprocess = "Union";
-		String dataType = "polygons";
+		String dataType = "Polygons";
 
 		//Retrieve data from db
 		Storage storageData = new Storage();
 		long startData = System.nanoTime();
-		String dataOne = storageData.fetchWKT(dataType, wktID);
-		String dataTwo = storageData.fetchWKT(dataType, wktID);
+		String dataOne = storageData.fetchWKT("UnionPoly", wktID);
+		String dataTwo = storageData.fetchWKT("UnionPoly", wktID);
 		long dataTime = (System.nanoTime()-startData)/1000000;
 		
 		//Length of input wkt (bytes and # nodes)
